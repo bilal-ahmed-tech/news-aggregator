328a021d0b7fb4d8a1c1167e68fb9586a6f1d3d1
Bilal Ahmed <bilalahmed19015@gmail.com>
2026-04-24 15:23:56 +0500

    fix: update NewsCard key to use article.link instead of article.url

--- components/ui/NewsGrid.tsx
+++ components/ui/NewsGrid.tsx
@@ -5,7 +5,7 @@ export default function NewsGrid({ articles }: { articles: Article[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {articles.map((article, index) => (
-        <NewsCard key={`${article.url}-${index}`} article={article} />
+        <NewsCard key={`${article.link}-${index}`} article={article} />
       ))}
     </div>
   );
